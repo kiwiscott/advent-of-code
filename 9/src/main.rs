3dbb1b58cab3509c50b98490d5aa2ec60d816562
kiwiscott <you@example.com>
2020-12-09 11:21:08 -0500

    Day 9

--- 9/src/main.rs
+++ 9/src/main.rs
@@ -1,52 +1,95 @@
-#[macro_use]
-extern crate lazy_static;
-
 use aoc::common::*;
+use std::collections::VecDeque; 
+use std::cmp::Ordering; 
 
 fn main() {
-    println!("Hello, world!");
+    let preamble_len = 25; 
+    let numbers: Vec<i64>= read_file("input.txt").unwrap()
+        .lines()
+        .map(|l| l.parse::<i64>().unwrap())
+        .collect();
 
-}
-#[derive(Debug, Copy, Clone)]
-enum Instruction {
-    Acc(OffSet),
-    Jmp(OffSet),
-    Nop(OffSet),
-    End,
-}
-#[derive(Debug, Copy, Clone)]
-enum OffSet {
-    Neg(usize),
-    Pos(usize),
-}
+    let p1 = match  problem1(preamble_len, numbers.clone()){
+        Some(n) => {
+            println!("Problem 1.Number [{:?}] is not a sum of its preamble numbers",n);
+            n
+        },
+        None => {
+                println!("Problem 1. All numbers are valid");
+                i64::MIN
+    }};
 
-fn parse(desc: String) -> Vec<Instruction> {
-    lazy_static! {
-        static ref RE: regex::Regex = regex::Regex::new(r"^(\w{3}).*([\+|-])(\d*)").unwrap();
+    if p1 == i64::MIN{
+       return; 
     }
 
-    let mut ps = Vec::<Instruction>::new();
 
-    for line in desc.lines() {
-        let caps = RE.captures(line).unwrap();
-
-        let abs = caps.get(3).unwrap().as_str().parse::<usize>().unwrap();
-
-        let os = match caps.get(2).unwrap().as_str() {
-            "+" => OffSet::Pos(abs),
-            "-" => OffSet::Neg(abs),
-            _ => panic!("Offset Not Expected"),
-        };
-
-        let op = match caps.get(1).unwrap().as_str() {
-            "nop" => Instruction::Nop(os),
-            "acc" => Instruction::Acc(os),
-            "jmp" => Instruction::Jmp(os),
-            _ => panic!("Type Not Expected"),
-        };
-        ps.push(op);
+    match problem2(numbers.clone(),p1){
+        Some(n) => println!("Problem 2: Sum {:?} [{:?}]",n.0+n.1,n), 
+        None => println!("Problem 2: All numbers are valid"), 
     }
-    ps.push(Instruction::End);
+    println!("Should BE: 1261309 [(408514, 852795)]");
 
-    ps
+}
+fn problem2(numbers: Vec<i64>, number_to_find: i64) -> Option<(i64,i64)>
+{
+    let mut nums_to_sum: VecDeque<i64> = VecDeque::new();
+    let mut result = (0 as i64, 0 as i64);
+
+    let mut index = 0; 
+    let mut sum = 0;
+    while index < numbers.len(){
+        match sum.cmp(&number_to_find) {
+            Ordering::Less => {
+                    nums_to_sum.push_back(numbers[index]);
+                    sum = sum + numbers[index];
+                    index = index+1;
+            },
+            Ordering::Greater => { 
+                sum = sum - nums_to_sum.pop_front().unwrap(); 
+            },
+            Ordering::Equal => {
+                let min = nums_to_sum.iter().map(|n|*n).min().unwrap();
+                let max = nums_to_sum.iter().map(|n|*n).max().unwrap();
+                result = (min,max);
+                println!("Result:{:?} s:{:?} nums:{:?}", number_to_find, sum, nums_to_sum );
+                break;
+            }
+        }
+    }
+
+    if result ==  (0 as i64, 0 as i64)
+    {
+        return None; 
+    }
+
+    Some(result)
+
+}
+
+
+
+fn problem1(preamble_length: usize,numbers: Vec<i64> ) -> Option<i64>{
+    //What is the first number that does cannot be represented by a sum of two numbers in the previous 'preamble' numbers? 
+    //Build to preamble 
+    let mut preamble: VecDeque<i64> = VecDeque::with_capacity(preamble_length);
+    numbers.iter().take(preamble_length).for_each(|n| preamble.push_back(*n));
+
+    //println!("Preamble {:?}-{:?}",preamble.len(),preamble);
+
+    
+    for n in numbers.iter().skip(preamble_length){
+        let found = preamble.iter().any(|a|{
+            preamble.contains(&(n-a))
+        });
+        if !found{
+            //println!("Looking for n:[{:?}] [{:?}] in {:?}", n,found ,preamble); 
+            return Some(*n);
+        }
+    
+        //push this to the back of our queue 
+        preamble.pop_front();   
+        preamble.push_back(*n);               
+    }
+    return None; 
 }
