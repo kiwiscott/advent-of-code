c41bf624a8c357726cd48d0f0fd930540b847339
kiwiscott <you@example.com>
2020-12-14 16:29:02 -0500

    Day 14

---
+++ 14/src/lib.rs
@@ -0,0 +1,27 @@
+pub mod common {
+    use std::fs::File;
+    use std::io::prelude::*;
+
+    pub fn read_file(fname: &str) -> std::io::Result<String> {
+        let mut file = File::open(fname)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
+        return Ok(content);
+    }
+    pub fn empty_line_split_into_single_line(desc: String) -> Vec<String> {
+        let mut ps = Vec::new();
+
+        let mut entry = String::new();
+        for line in desc.lines() {
+            if line.trim() == "" {
+                ps.push(entry);
+                entry = String::new();
+            } else {
+                entry = format!("{} {}", entry, line);
+            }
+        }
+        ps.push(entry);
+
+        ps
+    }
+}
